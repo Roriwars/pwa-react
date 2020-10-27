411dcf6ed43dffb7c21f0b77ce637b5af0126ef1
Camagny <robin2b2010@gmail.com>
2020-10-27 12:32:55 +0100

    Tuto React avec Firebase

---
+++ lists-pwa/src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCbPtm88vgbupJIFfj4gx4hK7D-CtoP6_g",
+    authDomain: "new-idea-3ced4.firebaseapp.com",
+    databaseURL: "https://new-idea-3ced4.firebaseio.com",
+    projectId: "new-idea-3ced4",
+    storageBucket: "new-idea-3ced4.appspot.com",
+    messagingSenderId: "504234513081",
+    appId: "1:504234513081:web:493407ca547116b3400168"
+};
